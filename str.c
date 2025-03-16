534e7547c825f4125f07d66f3dc149a32ad36511
Theeeccoo <thiagodecampos1@gmail.com>
2025-03-16 15:37:51 -0300

    [FIX] ~ Correcting realloc at deletion

--- str.c
+++ str.c
@@ -57,7 +57,7 @@ void string_delete_char_at(string_tt str, int position)
     assert( str != NULL && "ERROR: Appending into empty String\n");
     assert( ((position >= 0) && (position < (int) str->content_len)) && "ERROR: Invalid position to append.\n");
 
-    char* new_str = realloc(str->content, str->content_len + 2);
+    char* new_str = realloc(str->content, str->content_len - 1);
     if ( new_str == NULL ) handle_error("ERROR: Unnable to realloc string\n");
     str->content = new_str;
     assert( (memmove(str->content + position, str->content + position + 1, str->content_len - position + 1)) != NULL );
